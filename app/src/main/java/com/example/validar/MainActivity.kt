6cf262a64feee5c2c495c3a0e0ab3d524a5e23c9
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-05 02:32:53 -0300

    app funcionando

--- app/src/main/java/com/example/validar/MainActivity.kt
+++ app/src/main/java/com/example/validar/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.validar
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -39,10 +53,9 @@ fun MainScreen() {
         )
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Usar un Box para centrar el botón
         Box(
             modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center // Centrar el contenido
+            contentAlignment = Alignment.Center
         ) {
             Button(onClick = {
                 dialogMessage = if (text == "123456") {
@@ -57,7 +70,6 @@ fun MainScreen() {
         }
     }
 
-    // AlertDialog para mostrar el resultado
     if (showDialog) {
         ValidationDialog(
             message = dialogMessage,
